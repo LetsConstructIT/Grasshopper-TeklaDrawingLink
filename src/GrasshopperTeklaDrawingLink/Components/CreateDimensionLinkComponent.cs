54738f8066203c63565df884b15f6a68bb45e6f4
Greg <grzegorz.olszewski123@gmail.com>
2023-07-16 19:09:57 +0200

    CreateDimensionLinkComponent is also the 'Createable' type of component

--- src/GrasshopperTeklaDrawingLink/Components/CreateDimensionLinkComponent.cs
+++ src/GrasshopperTeklaDrawingLink/Components/CreateDimensionLinkComponent.cs
@@ -3,13 +3,14 @@ using GTDrawingLink.Extensions;
 using GTDrawingLink.Tools;
 using GTDrawingLink.Types;
 using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using Tekla.Structures.Drawing;
 
 namespace GTDrawingLink.Components
 {
-    public class CreateDimensionLinkComponent : TeklaComponentBase
+    public class CreateDimensionLinkComponent : CreateDatabaseObjectComponentBase
     {
         public override GH_Exposure Exposure => GH_Exposure.primary;
         protected override Bitmap Icon => Properties.Resources.CreateDimensionLink;
@@ -28,12 +29,13 @@ namespace GTDrawingLink.Components
             pManager.AddParameter(new TeklaDatabaseObjectParam(ParamInfos.StraightDimensionSet, GH_ParamAccess.list));
         }
 
-        protected override void SolveInstance(IGH_DataAccess DA)
+        protected override IEnumerable<DatabaseObject> InsertObjects(IGH_DataAccess DA)
         {
             var straightDimensions = DA.GetGooListValue<DatabaseObject>(ParamInfos.StraightDimensionSet).Cast<StraightDimensionSet>().ToList();
             if (straightDimensions == null)
-                return;
+                return null;
 
+            var dimensionLinks = new List<DimensionLink>();
             for (int i = 0; i < straightDimensions.Count; i++)
             {
                 for (int j = 0; j < straightDimensions.Count; j++)
@@ -44,14 +46,17 @@ namespace GTDrawingLink.Components
                     if (AreParallel(straightDimensions[i], straightDimensions[j]))
                         continue;
 
-                    new DimensionLink(straightDimensions[i], straightDimensions[j])
-                        .Insert();
+                    var dimensionLink = new DimensionLink(straightDimensions[i], straightDimensions[j]);
+                    dimensionLinks.Add(dimensionLink);
                 }
             }
+            dimensionLinks.ForEach(d => d.Insert());
 
             DrawingInteractor.CommitChanges();
 
             DA.SetDataList(ParamInfos.StraightDimensionSet.Name, straightDimensions);
+
+            return dimensionLinks;
         }
 
         private bool AreParallel(StraightDimensionSet straightDimensionSet1, StraightDimensionSet straightDimensionSet2)
