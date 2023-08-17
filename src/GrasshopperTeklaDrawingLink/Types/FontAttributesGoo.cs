f06e34a87c7de7a084c3db8d0ae31d01dc2a4b01
Greg <grzegorz.olszewski123@gmail.com>
2023-08-17 22:11:23 +0200

    More verbose FontAttributesGoo ToString method

--- src/GrasshopperTeklaDrawingLink/Types/FontAttributesGoo.cs
+++ src/GrasshopperTeklaDrawingLink/Types/FontAttributesGoo.cs
@@ -4,37 +4,45 @@ using System;
 
 using Tekla.Structures.Drawing;
 
-namespace GTDrawingLink.Types {
-    public class FontAttributesGoo : GH_Goo<FontAttributes> {
+namespace GTDrawingLink.Types
+{
+    public class FontAttributesGoo : GH_Goo<FontAttributes>
+    {
         public override bool IsValid => true;
 
         public override string TypeDescription => "Tekla font attributes";
 
         public override string TypeName => typeof(FontAttributes).ToShortString();
 
-        public FontAttributesGoo() {
+        public FontAttributesGoo()
+        {
         }
 
         public FontAttributesGoo(FontAttributes attr)
-            : base(attr) {
+            : base(attr)
+        {
         }
-        public override IGH_Goo Duplicate() {
+        public override IGH_Goo Duplicate()
+        {
             throw new NotImplementedException();
         }
 
-        public override bool CastFrom(object source) {
-            if(source is FontAttributes) {
-                Value=source as FontAttributes;
+        public override bool CastFrom(object source)
+        {
+            if (source is FontAttributes)
+            {
+                Value = source as FontAttributes;
                 return true;
             }
             return base.CastFrom(source);
         }
 
-        public override string ToString() {
-            if(Value==null)
+        public override string ToString()
+        {
+            if (Value == null)
                 return "No value";
-            //I'm not sure what this is for
-            return $"{Value.Name}";
+
+            return $"{Value.Color} {Value.Name} {Value.Height} b: {Value.Bold} i: {Value.Italic}";
         }
     }
 }
