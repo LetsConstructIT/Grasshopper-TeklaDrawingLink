7f5b44c99aaf668eebf5b22d57b6dd7ff8485a94
Greg <grzegorz.olszewski123@gmail.com>
2023-08-20 21:29:32 +0200

    Using Frame in Text Attributes

--- src/GrasshopperTeklaDrawingLink/Components/Text/TextAttributesComponent.cs
+++ src/GrasshopperTeklaDrawingLink/Components/Text/TextAttributesComponent.cs
@@ -16,15 +16,15 @@ namespace GTDrawingLink.Components.Text
 
         protected override void InvokeCommand(IGH_DataAccess DA)
         {
-            var (fontAttributes, frameType, frameColor, arrowAttributes, backgroundTransparency, angle, rulerWidth, attributesName) = _command.GetInputValues();
+            var (fontAttributes, frame, arrowAttributes, backgroundTransparency, angle, rulerWidth, attributesName) = _command.GetInputValues();
 
             var textAttributes = new TSD.Text.TextAttributes(attributesName);
 
             if (fontAttributes != null)
                 textAttributes.Font = fontAttributes;
 
-            if (frameType != null)
-                textAttributes.Frame = new Frame(frameType.Value, frameColor);
+            if (frame != null)
+                textAttributes.Frame = frame;
 
             if (arrowAttributes != null)
                 textAttributes.ArrowHead = arrowAttributes;
@@ -44,8 +44,7 @@ namespace GTDrawingLink.Components.Text
     public class TextAttributesCommand : CommandBase
     {
         private readonly InputOptionalParam<FontAttributes> _inFontAttributes = new InputOptionalParam<FontAttributes>(ParamInfos.FontAttributes);
-        private readonly InputOptionalStructParam<FrameTypes> _inFrameType = new InputOptionalStructParam<FrameTypes>(ParamInfos.FrameType);
-        private readonly InputOptionalStructParam<DrawingColors> _inFrameColor = new InputOptionalStructParam<DrawingColors>(ParamInfos.DrawingColor, DrawingColors.Black);
+        private readonly InputOptionalParam<Frame> _inFrameType = new InputOptionalParam<Frame>(ParamInfos.FrameAtributes);
         private readonly InputOptionalParam<ArrowheadAttributes> _inArrowAttributes = new InputOptionalParam<ArrowheadAttributes>(ParamInfos.ArrowAttribute);
         private readonly InputOptionalStructParam<bool> _inBackgroundTransparency = new InputOptionalStructParam<bool>(ParamInfos.BackgroundTransparency);
         private readonly InputOptionalStructParam<double> _inAngle = new InputOptionalStructParam<double>(ParamInfos.Angle);
@@ -54,12 +53,11 @@ namespace GTDrawingLink.Components.Text
 
         private readonly OutputParam<TSD.Text.TextAttributes> _outAttributes = new OutputParam<TSD.Text.TextAttributes>(ParamInfos.TextAttributes);
 
-        internal (FontAttributes? fontAttributes, FrameTypes? frameType, DrawingColors frameColor, ArrowheadAttributes? arrowAttributes, bool? backgroundTransparency, double? angle, double? rulerWidth, string? attributesName) GetInputValues()
+        internal (FontAttributes? fontAttributes, Frame? frame, ArrowheadAttributes? arrowAttributes, bool? backgroundTransparency, double? angle, double? rulerWidth, string? attributesName) GetInputValues()
         {
             return (
                 _inFontAttributes.ValueProvidedByUser ? _inFontAttributes.Value : null,
-                _inFrameType.ValueProvidedByUser ? _inFrameType.Value : new FrameTypes?(),
-                _inFrameColor.Value,
+                _inFrameType.GetValueFromUserOrNull(),
                 _inArrowAttributes.ValueProvidedByUser ? _inArrowAttributes.Value : null,
                 _inBackgroundTransparency.ValueProvidedByUser ? _inBackgroundTransparency.Value : new bool?(),
                 _inAngle.ValueProvidedByUser ? _inAngle.Value : new double?(),
