e966591bb0f38dc06c70cab082a371f69970d97e
Greg <grzegorz.olszewski123@gmail.com>
2023-08-21 19:01:38 +0200

    New version of CreateDatabaseObjectComponentBase

---
+++ src/GrasshopperTeklaDrawingLink/Components/CreateDatabaseObjectComponentAttributesNew.cs
@@ -0,0 +1,34 @@
+﻿using Grasshopper.Kernel.Attributes;
+using GTDrawingLink.Tools;
+
+namespace GTDrawingLink.Components
+{
+    public class CreateDatabaseObjectComponentAttributesNew<T> : GH_ComponentAttributes where T : CommandBase, new()
+    {
+        public override bool Selected
+        {
+            get => base.Selected;
+            set
+            {
+                if (value != Selected)
+                {
+                    var createModelObjectComponent = base.Owner as CreateDatabaseObjectComponentBaseNew<T>;
+                    if (value)
+                    {
+                        createModelObjectComponent.HighlightObjects();
+                    }
+                    else
+                    {
+                        createModelObjectComponent.UnHighlightObjects();
+                    }
+                }
+                base.Selected = value;
+            }
+        }
+
+        public CreateDatabaseObjectComponentAttributesNew(CreateDatabaseObjectComponentBaseNew<T> owner)
+            : base(owner)
+        {
+        }
+    }
+}
