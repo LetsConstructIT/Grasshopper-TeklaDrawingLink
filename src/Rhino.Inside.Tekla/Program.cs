1b7acfcc79039c1ebd08b8941645e5ed6cc2b941
Greg <grzegorz.olszewski123@gmail.com>
2024-01-17 20:29:27 +0100

    First try with the Rhino.Inside

---
+++ src/Rhino.Inside.Tekla/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Rhino.Inside.Tekla
+{
+    internal class Program
+    {
+        static Program()
+        {
+            RhinoInside.Resolver.Initialize();
+        }
+
+        static void Main(string[] args)
+        {
+            using (var core = new Runtime.InProcess.RhinoCore())
+            {
+                RunHelper();
+            }
+        }
+        static void RunHelper()
+        {
+            string filePath = @"C:\Users\grzeg\OneDrive\Desktop\Line.gh";
+
+            var pluginObject = RhinoApp.GetPlugInObject("Grasshopper") as Grasshopper.Plugin.GH_RhinoScriptInterface;
+            pluginObject.RunHeadless();
+
+            var io = new Grasshopper.Kernel.GH_DocumentIO();
+            if (!io.Open(filePath))
+                Console.WriteLine("File loading failed.");
+            else
+            {
+                var doc = io.Document;
+                doc.Enabled = true;
+                doc.NewSolution(false);
+            }
+
+            Console.WriteLine("Done... press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
