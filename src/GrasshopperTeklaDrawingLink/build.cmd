da7db22eadae0c22455c21689fee6973f1b751cb
Greg <grzegorz.olszewski123@gmail.com>
2024-01-30 22:11:15 +0100

    Build script for multiple Tekla versions

---
+++ src/GrasshopperTeklaDrawingLink/build.cmd
@@ -0,0 +1,11 @@
+rmdir .\bin /s /q
+
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2019 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2020 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2021 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2022 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2023 /t:Rebuild
+
+for /R .\bin %%f in (*.gha) do copy %%f .\bin\
+
+echo
