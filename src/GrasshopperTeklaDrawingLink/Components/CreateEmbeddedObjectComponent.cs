a7a898be163044b6d5ca8187603bf9ef81326e54
Greg <grzegorz.olszewski123@gmail.com>
2024-02-11 14:23:17 +0100

    Create DWG object component works on single insertion

--- src/GrasshopperTeklaDrawingLink/Components/CreateEmbeddedObjectComponent.cs
+++ src/GrasshopperTeklaDrawingLink/Components/CreateEmbeddedObjectComponent.cs
@@ -1,14 +1,10 @@
 ﻿using Grasshopper.Kernel;
-using Grasshopper.Kernel.Types;
 using GTDrawingLink.Extensions;
 using GTDrawingLink.Tools;
 using Rhino.Geometry;
 using System.Collections.Generic;
 using System.Drawing;
-using System.Linq;
 using Tekla.Structures.Drawing;
-using TSD = Tekla.Structures.Drawing;
-using TSG = Tekla.Structures.Geometry3d;
 
 namespace GTDrawingLink.Components
 {
@@ -21,50 +17,38 @@ namespace GTDrawingLink.Components
 
         protected override IEnumerable<DatabaseObject> InsertObjects(IGH_DataAccess DA)
         {
-            (ViewBase View, List<Point3d> Points, string FileName, EmbeddedObjectAttributes Attributes) = _command.GetInputValues();
+            (ViewBase View, Point3d Point, string FileName, EmbeddedObjectAttributes Attributes) = _command.GetInputValues();
 
-            var dwgs = new List<DwgObject>();
-            if (Points.Count == 0)
-            {
-                AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "The specified point group could not be parsed");
-                return dwgs;
-            }
+            var dwg = new DwgObject(View, Point.ToTekla(), FileName, Attributes);
+            dwg.Insert();
 
-            foreach (var point in Points)
-            {
-                var line = new DwgObject(View, point.ToTekla(), FileName, Attributes);
-                line.Insert();
-
-                dwgs.Add(line);
-            }
-
-            _command.SetOutputValues(DA, dwgs);
+            _command.SetOutputValues(DA, dwg);
 
             DrawingInteractor.CommitChanges();
-            return dwgs;
+            return new List<DatabaseObject>() { dwg };
         }
     }
 
     public class CreateEmbeddedObjectCommand : CommandBase
     {
         private readonly InputParam<ViewBase> _inView = new InputParam<ViewBase>(ParamInfos.ViewBase);
-        private readonly InputListPoint _inPoints = new InputListPoint(ParamInfos.Points);
+        private readonly InputPoint _inPoint = new InputPoint(ParamInfos.InsertionPoint);
         private readonly InputParam<string> _inFileName = new InputParam<string>(ParamInfos.DwgFileName);
         private readonly InputParam<EmbeddedObjectAttributes> _inAttributes = new InputParam<EmbeddedObjectAttributes>(ParamInfos.DwgAttributes);
 
-        private readonly OutputListParam<DwgObject> _outDwgs = new OutputListParam<DwgObject>(ParamInfos.Dwg);
+        private readonly OutputParam<DwgObject> _outDwgs = new OutputParam<DwgObject>(ParamInfos.Dwg);
 
-        internal (ViewBase View, List<Point3d> points, string fileName, EmbeddedObjectAttributes attributes) GetInputValues()
+        internal (ViewBase View, Point3d point, string fileName, EmbeddedObjectAttributes attributes) GetInputValues()
         {
             return (_inView.Value,
-                    _inPoints.Value,
+                    _inPoint.Value,
                     _inFileName.Value,
                     _inAttributes.Value);
         }
 
-        internal Result SetOutputValues(IGH_DataAccess DA, List<DwgObject> dwgs)
+        internal Result SetOutputValues(IGH_DataAccess DA, DwgObject dwg)
         {
-            _outDwgs.Value = dwgs;
+            _outDwgs.Value = dwg;
 
             return SetOutput(DA);
         }
