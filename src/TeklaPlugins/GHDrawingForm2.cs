568f4e4b8cc2760c0c5f55f87898434980e5d6fa
Greg <grzegorz.olszewski123@gmail.com>
2024-02-17 19:10:27 +0100

    Naive approach for Tekla plugins

---
+++ src/TeklaPlugins/GHDrawingForm2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tekla.Structures.Dialog;
+
+namespace TeklaPlugins
+{
+    public partial class GHDrawingForm2 : PluginFormBase
+    {
+        public GHDrawingForm2()
+        {
+            InitializeComponent();
+        }
+
+        private void okCancel1_OkClicked(object sender, EventArgs e)
+        {
+            this.Modify();
+        }
+
+        private void okCancel1_CancelClicked(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
---
+++ src/TeklaPlugins/ModelPlugin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tekla.Structures.Plugins;
+
+namespace TeklaPlugins
+{
+    [Plugin("GHDrawing")]
+    [PluginUserInterface("TeklaPlugins.GHDrawingForm")]
+    [InputObjectDependency(InputObjectDependency.NOT_DEPENDENT)]
+    public class ModelPlugin : PluginBase
+    {
+        public override List<InputDefinition> DefineInput()
+        {
+            return new List<InputDefinition>();
+        }
+
+        public override bool Run(List<InputDefinition> Input)
+        {
+            Tekla.Structures.Model.Operations.Operation.DisplayPrompt("I'm inside model");
+
+
+            return true;
+        }
+    }
+}
