6535d7ceeb54aa3b6477436c41a3dcfcf3539e88
Greg <grzegorz.olszewski123@gmail.com>
2024-02-22 17:05:31 +0100

    GetDrawingPropertiesComponent returns also sheet number

--- src/GrasshopperTeklaDrawingLink/Components/GetDrawingPropertiesComponent.cs
+++ src/GrasshopperTeklaDrawingLink/Components/GetDrawingPropertiesComponent.cs
@@ -1,5 +1,6 @@
 ﻿using Grasshopper.Kernel;
 using GTDrawingLink.Tools;
+using System;
 using System.Drawing;
 using Tekla.Structures.Drawing;
 
@@ -18,7 +19,19 @@ namespace GTDrawingLink.Components
 
             var sheetSize = drawing.Layout.SheetSize;
 
-            _command.SetOutputValues(DA, drawing.Name, drawing.Title1, drawing.Title2, drawing.Title3, sheetSize.Width, sheetSize.Height);
+            var sheetNumber = GetSheetNumber(drawing);
+            _command.SetOutputValues(DA, drawing.Name, drawing.Title1, drawing.Title2, drawing.Title3, sheetNumber, sheetSize.Width, sheetSize.Height);
+        }
+
+        private int GetSheetNumber(Drawing drawing)
+        {
+            return drawing switch
+            {
+                CastUnitDrawing cuDrawing => cuDrawing.SheetNumber,
+                AssemblyDrawing assemblyDrawing => assemblyDrawing.SheetNumber,
+                SinglePartDrawing singlePartDrawing => singlePartDrawing.SheetNumber,
+                _ => 0
+            };
         }
     }
 
@@ -29,6 +42,7 @@ namespace GTDrawingLink.Components
         private readonly OutputParam<string> _outTitle1 = new OutputParam<string>(new GH_InstanceDescription("Title1", "T1", "Drawing title 1", "", ""));
         private readonly OutputParam<string> _outTitle2 = new OutputParam<string>(new GH_InstanceDescription("Title2", "T2", "Drawing title 2", "", ""));
         private readonly OutputParam<string> _outTitle3 = new OutputParam<string>(new GH_InstanceDescription("Title3", "T3", "Drawing title 3", "", ""));
+        private readonly OutputParam<int> _outSheetNumber = new OutputParam<int>(new GH_InstanceDescription("Sheet No", "N", "Sheet number", "", ""));
         private readonly OutputParam<double> _outWidth = new OutputParam<double>(new GH_InstanceDescription("Width", "W", "Drawing width", "", ""));
         private readonly OutputParam<double> _outHeight = new OutputParam<double>(new GH_InstanceDescription("Height", "H", "Drawing height", "", ""));
 
@@ -37,12 +51,13 @@ namespace GTDrawingLink.Components
             return _inDrawing.Value;
         }
 
-        internal Result SetOutputValues(IGH_DataAccess DA, string name, string title1, string title2, string title3, double width, double height)
+        internal Result SetOutputValues(IGH_DataAccess DA, string name, string title1, string title2, string title3, int sheetNumber, double width, double height)
         {
             _outName.Value = name;
             _outTitle1.Value = title1;
             _outTitle2.Value = title2;
             _outTitle3.Value = title3;
+            _outSheetNumber.Value = sheetNumber;
             _outWidth.Value = width;
             _outHeight.Value = height;
 
