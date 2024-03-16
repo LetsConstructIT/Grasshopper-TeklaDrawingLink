5a232c3e7ead744a801e88797e9f3326ba66c206
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-03-16 12:23:02 +0100

    Objects from View returns keys as a tree with single key in branch

--- src/GrasshopperTeklaDrawingLink/Tools/VersionSpecificConstants.cs
+++ src/GrasshopperTeklaDrawingLink/Tools/VersionSpecificConstants.cs
@@ -305,6 +305,10 @@ namespace GTDrawingLink.Tools
                 "0F3832E1-2D16-4C56-96E1-2F63C1C5FFAC"
             },
             {
+                typeof(GetObjectsFromViewComponent),
+                "B8BF5B8F-F577-4543-8B39-E3F1B84AC2E0"
+            },
+            {
                 typeof(GetObjectsFromViewComponentOLD),
                 "D340933C-5C78-4052-A986-777FAE8241B0"
             },
