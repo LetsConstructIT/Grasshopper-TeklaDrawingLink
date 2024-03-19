d06d9bc718fafe1812d711951b7d83548dabb0f3
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-03-19 17:12:47 +0100

    Edit mode moved to next ribbon section

--- src/GrasshopperTeklaDrawingLink/Components/GetEditModeComponent.cs
+++ src/GrasshopperTeklaDrawingLink/Components/GetEditModeComponent.cs
@@ -6,7 +6,7 @@ namespace GTDrawingLink.Components
 {
     public class GetEditModeComponent : TeklaComponentBaseNew<GetEditModeCommand>
     {
-        public override GH_Exposure Exposure => GH_Exposure.quarternary;
+        public override GH_Exposure Exposure => GH_Exposure.quinary;
         protected override Bitmap Icon => Properties.Resources.GetEditMode;
 
         public GetEditModeComponent() : base(ComponentInfos.GetEditModeComponent) { }
