71d64c1a671a5515bdc9846113bdb8296f83c36a
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-03-19 19:27:11 +0100

    Removed not used drawings types from TeklaDatabaseObjectParam

--- src/GrasshopperTeklaDrawingLink/Types/TeklaDrawingPartParam.cs
+++ src/GrasshopperTeklaDrawingLink/Types/TeklaDrawingPartParam.cs
@@ -1,7 +1,6 @@
 ﻿using Grasshopper.Kernel;
 using GTDrawingLink.Tools;
 using System.Drawing;
-using Tekla.Structures.Drawing;
 
 namespace GTDrawingLink.Types
 {
@@ -10,12 +9,12 @@ namespace GTDrawingLink.Types
         public override GH_Exposure Exposure => GH_Exposure.hidden;
         protected override Bitmap Icon => Properties.Resources.DrawingPart;
 
-        public TeklaDrawingPartParam() : base(ComponentInfos.DrawingPartParam, typeof(Part))
+        public TeklaDrawingPartParam() : base(ComponentInfos.DrawingPartParam)
         {
         }
 
         public TeklaDrawingPartParam(GH_InstanceDescription tag, GH_ParamAccess access)
-            : base(tag, typeof(Part))
+            : base(tag)
         {
             Access = access;
         }
