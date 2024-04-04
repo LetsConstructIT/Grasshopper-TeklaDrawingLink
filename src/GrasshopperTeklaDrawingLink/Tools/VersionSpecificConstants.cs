e2f41a42323147e6fc7c2e66fda24358d6ef3b35
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-04-04 21:35:42 +0200

    Related objects returns the same key-tree structure as Group Objects and Get Objects from View

--- src/GrasshopperTeklaDrawingLink/Tools/VersionSpecificConstants.cs
+++ src/GrasshopperTeklaDrawingLink/Tools/VersionSpecificConstants.cs
@@ -530,6 +530,10 @@ namespace GTDrawingLink.Tools
                 "F0939985-9E2E-4677-957F-F56C07AE6BEF"
             },
             {
+                typeof(GetRelatedObjectsComponent),
+                "C053740C-C6DC-4D64-BDA0-0DB69E33C765"
+            },
+            {
                 typeof(GetRelatedObjectsComponentOLD),
                 "597CE529-4306-4F4E-BF80-01C93E3613A5"
             },
