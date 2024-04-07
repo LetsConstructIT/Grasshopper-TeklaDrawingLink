46281fdb902946e6637c1b38bcd213a23d4dba22
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-04-07 21:06:52 +0200

    Version upgrade

--- src/GrasshopperTeklaDrawingLink/Properties/AssemblyInfo.cs
+++ src/GrasshopperTeklaDrawingLink/Properties/AssemblyInfo.cs
@@ -11,4 +11,4 @@ using System.Runtime.InteropServices;
 
 [assembly: Guid("75ab53a8-c767-415d-9c27-f4c1610fe8c1")]
 
-[assembly: AssemblyVersion("2.10.0.*")]
+[assembly: AssemblyVersion("2.11.0.*")]
