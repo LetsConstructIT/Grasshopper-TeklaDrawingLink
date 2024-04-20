a4119806d0ddcdb1f1a848a1d47ec83e957886ea
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-04-20 17:54:10 +0200

    Drawing Plugins not properly inserted can be removed

--- src/GrasshopperTeklaDrawingLink/Components/Plugins/CreatePluginComponent.cs
+++ src/GrasshopperTeklaDrawingLink/Components/Plugins/CreatePluginComponent.cs
@@ -9,6 +9,7 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 using Tekla.Structures.Drawing;
+using Tekla.Structures.DrawingInternal;
 
 namespace GTDrawingLink.Components.Plugins
 {
@@ -59,8 +60,16 @@ namespace GTDrawingLink.Components.Plugins
                                           pluginAttribute,
                                           objectsToSelect.GetBranch(i));
 
-                outputObjects.Add(plugin);
-                outputTree.Append(new TeklaDatabaseObjectGoo(plugin), path);
+                if (plugin.GetIdentifier().ID == 0)
+                {
+                    AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Drawing plugin was not properly inserted");
+                    outputTree.Append(new TeklaDatabaseObjectGoo(null), path);
+                }
+                else
+                {
+                    outputObjects.Add(plugin);
+                    outputTree.Append(new TeklaDatabaseObjectGoo(plugin), path);
+                }
             }
 
             _command.SetOutputValues(DA, outputTree);
