9d3c6bae5efab3054e98651cf2eb6e1ac7fff4ff
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-04-21 09:52:09 +0200

    Order Dimension Lines loads at the beginning current position of dimension line

--- src/GrasshopperTeklaDrawingLink/Components/Dimensions/OrderStraightDimensionSetComponent.cs
+++ src/GrasshopperTeklaDrawingLink/Components/Dimensions/OrderStraightDimensionSetComponent.cs
@@ -24,6 +24,7 @@ namespace GTDrawingLink.Components.Dimensions
             }
             else
             {
+                dimensionLines.ForEach(d => d.Select());
                 OrderDimensions(dimensionLines, distance);
             }
 
