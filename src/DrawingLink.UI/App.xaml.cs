b165cc824236911f310b8bcbe564dc681915fc17
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-10-24 18:30:22 +0200

    Handling case when there is only Rhino 8 installed

--- src/DrawingLink.UI/App.xaml.cs
+++ src/DrawingLink.UI/App.xaml.cs
@@ -31,6 +31,8 @@ namespace DrawingLink.UI
 
         private List<Rhino.RhinoInfo> SetupRhinoFolders()
         {
+            var versionToLaunch = UI.Properties.Settings.Default.RhinoVersion;
+
             var versions = Rhino.RhinoInfo.GetAllVersions();
             if (versions.Count == 0)
             {
@@ -38,7 +40,18 @@ namespace DrawingLink.UI
                 Environment.Exit(0);
             }
 
-            var neededVersion = versions.Where(v => v.Version == UI.Properties.Settings.Default.RhinoVersion).OrderByDescending(v => v.ServiceRelease).FirstOrDefault();
+            var availableVersions = versions.Select(v => v.Version).Distinct().ToList();
+            if (availableVersions.Count == 1)
+            {
+                versionToLaunch = versions.First().Version;
+                UI.Properties.Settings.Default.RhinoVersion = versionToLaunch;
+            }
+
+            var neededVersion = versions
+                .Where(v => v.Version == versionToLaunch)
+                .OrderByDescending(v => v.ServiceRelease)
+                .FirstOrDefault();
+
             if (neededVersion != null)
                 _rhinoFolders = neededVersion.InstallFolders.ToArray();
 
