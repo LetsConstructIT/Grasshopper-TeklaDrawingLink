b11227b224a0a8019555614425b371a1da448bca
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2024-10-30 19:11:36 +0100

    Set of three export icons

--- src/GrasshopperTeklaDrawingLink/Properties/Resources.Designer.cs
+++ src/GrasshopperTeklaDrawingLink/Properties/Resources.Designer.cs
@@ -553,6 +553,36 @@ namespace GTDrawingLink.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap ExportIfc {
+            get {
+                object obj = ResourceManager.GetObject("ExportIfc", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ExportPdf {
+            get {
+                object obj = ResourceManager.GetObject("ExportPdf", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap ExportReport {
+            get {
+                object obj = ResourceManager.GetObject("ExportReport", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap FindVisibleEdges {
             get {
                 object obj = ResourceManager.GetObject("FindVisibleEdges", resourceCulture);
