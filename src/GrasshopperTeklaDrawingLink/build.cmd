889ad12742af75c81f73887342957f7e8f79bf88
Grzegorz Olszewski <grzegorz.olszewski123@gmail.com>
2025-03-26 07:58:01 +0100

    Renaming project configurations by removing underscore at the beginning

--- src/GrasshopperTeklaDrawingLink/build.cmd
+++ src/GrasshopperTeklaDrawingLink/build.cmd
@@ -1,13 +1,13 @@
 rmdir .\bin /s /q
 
-msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2019 /t:Rebuild
-msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2019i /t:Rebuild
-msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2020 /t:Rebuild
-msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2021 /t:Rebuild
-msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2022 /t:Rebuild
-msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2023 /t:Rebuild
-msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2024 /t:Rebuild
-msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=_2025 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=2019 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=2019i /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=2020 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=2021 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=2022 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=2023 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=2024 /t:Rebuild
+msbuild GrasshopperTeklaDrawingLink.csproj /t:restore -p:Configuration=2025 /t:Rebuild
 
 for /R .\bin %%f in (*.gha) do copy %%f .\bin\
 
